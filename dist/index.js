5620011c1761410be9224b151b73835291f9f417
k1-searchbank <89293261+k1-searchbank@users.noreply.github.com>
2021-08-21 18:24:16 +0900

    create index.js

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+function fmtDate(dateStr) {
+  var ary = dateStr.split(/\/0?/g)
+  return `${ary[0]}/${('00' + ary[1]).slice(-2)}/${('00' + ary[2]).slice(-2)}`
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('consoleを開いたね？？？\nSearch Bankへようこそ！');
+
+  var publishedNode = document.querySelector('#published-date');
+  if (!publishedNode) return;
+
+  var published = fmtDate(publishedNode.content.textContent);
+  var updated = fmtDate(document.querySelector('.basicInfo .date').textContent.slice(-10));
+  if (!!published && published != updated) document.querySelector('.basicInfo .date').textContent += ` (公開日: ${published})`;
+});
